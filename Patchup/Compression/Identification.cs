eb1dddf407ab96e97cbe80f1b6296ffc241c8765
Ben Mitchell <dezyh@outlook.com>
2019-03-06 16:36:26 +1100

    Add compression identification for .png files

---
+++ Patchup/Compression/Identification.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Patchup.Compression
+{
+    public static class Identification
+    {
+        public static bool Png(byte[] data)
+        {
+            byte[] identifier = new byte[] {137, 80, 78, 71, 13, 10, 26, 10};
+
+            for (int i = 0; i < identifier.Length; i++)
+            {
+                if (data[i] != identifier[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Zip(byte[] data)
+        {
+            byte[] identifier = new byte[] {80, 75, 3, 4};
+
+            for (int i = 0; i < identifier.Length; i++)
+            {
+                if (data[i] != identifier[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
